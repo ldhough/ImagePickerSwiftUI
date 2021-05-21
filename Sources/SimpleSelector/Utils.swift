a555c16c3e05006b3b6da4da777143bfbfb186b9
ldhough <ldhough2000@gmail.com>
2021-05-20 23:18:13 -0400

    added view extension util

--- Sources/SimpleSelector/Utils.swift
+++ Sources/SimpleSelector/Utils.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 // Used to account for floating point math errors
 private func fp_eq<T: BinaryFloatingPoint>(_ lhs: T, _ rhs: T, tolerance: Float = 0.0001) -> Bool {
@@ -14,4 +15,14 @@ private func fp_eq<T: BinaryFloatingPoint>(_ lhs: T, _ rhs: T, tolerance: Float 
 
 private func fp_gte<T: BinaryFloatingPoint>(_ lhs: T, _ rhs: T) -> Bool {
     return round(1000*lhs)/1000 >= rhs
+}
+
+private extension View {
+    // Given the size frame enclosing a view, return optimal size of that view
+    // Takes modifiers like .fit, .fill into account
+    func viewDimensionsInFrame(_ frame: CGSize) -> CGSize {
+        let controller = UIHostingController(rootView: self)
+        let viewDimensions = controller.view.systemLayoutSizeFitting(frame)
+        return viewDimensions
+    }
 }
