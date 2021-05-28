255a8d79e3174e7092c81006a00dda1b90a92c05
ldhough <ldhough2000@gmail.com>
2021-05-28 18:29:32 -0400

    added mutex trylock

--- Sources/SimpleSelector/Utils.swift
+++ Sources/SimpleSelector/Utils.swift
@@ -9,15 +9,15 @@ import Foundation
 import SwiftUI
 
 // Used to account for floating point math errors
-private func fp_eq<T: BinaryFloatingPoint>(_ lhs: T, _ rhs: T, tolerance: Float = 0.0001) -> Bool {
+internal func fp_eq<T: BinaryFloatingPoint>(_ lhs: T, _ rhs: T, tolerance: Float = 0.0001) -> Bool {
     return abs(Float(lhs) - Float(rhs)) <= tolerance
 }
 
-private func fp_gte<T: BinaryFloatingPoint>(_ lhs: T, _ rhs: T) -> Bool {
+internal func fp_gte<T: BinaryFloatingPoint>(_ lhs: T, _ rhs: T) -> Bool {
     return round(1000*lhs)/1000 >= rhs
 }
 
-private extension View {
+internal extension View {
     // Given the size frame enclosing a view, return optimal size of that view
     // Takes modifiers like .fit, .fill into account
     func viewDimensionsInFrame(_ frame: CGSize) -> CGSize {
@@ -25,4 +25,27 @@ private extension View {
         let viewDimensions = controller.view.systemLayoutSizeFitting(frame)
         return viewDimensions
     }
+}
+
+internal class ImageLock {
+    
+    private var lock:pthread_mutex_t = pthread_mutex_t()
+    
+    init() {
+        pthread_mutex_init(&lock, nil)
+    }
+    
+    public func tryGetLock() -> Bool {
+        let result = pthread_mutex_trylock(&lock)
+        return result == 0
+    }
+    
+    public func releaseLock() {
+        pthread_mutex_unlock(&lock)
+    }
+    
+    deinit {
+        pthread_mutex_destroy(&lock)
+    }
+    
 }
